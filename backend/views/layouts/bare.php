d0cf1c8bbf4cbe252949ee0d64bce20805d2ad6c
HarishST <harishstprojects@gmail.com>
2020-06-20 22:55:22 +0530

    Bare Template Alert Included. Todo: Optimizations

--- backend/views/layouts/bare.php
+++ backend/views/layouts/bare.php
@@ -31,6 +31,35 @@ AppAsset::register($this);
 
 <?php $this->beginBody() ?>
 
+<!-- Alerts -->
+<?php if (Yii::$app->session->hasFlash('info')): ?>
+    <div class="alert alert-info mb-0">
+        <?= Yii::$app->session->getFlash('info') ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+<?php endif; ?>
+
+<?php if (Yii::$app->session->hasFlash('error')): ?>
+    <div class="alert alert-danger mb-0">
+        <?= Yii::$app->session->getFlash('error') ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+<?php endif; ?>
+
+<?php if (Yii::$app->session->hasFlash('success')): ?>
+    <div class="alert alert-success mb-0">
+        <?= Yii::$app->session->getFlash('success') ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+<?php endif; ?>
+<!-- Alerts End-->
+
 <div class="wrapper wrapper-full-page" style="background-image: url(<?= \Yii::getAlias('@web/img/cover.jpg'); ?>); background-size: cover; background-position: top center;">
 
     <div class="page-header header-filter">
